9418a9d5538d684b79dcb9abcd353f92ddcd164c
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-03-05 15:21:35 -0500

    f   - calculate number of words in a book

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from stats import get_num_words
+
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
 
-
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
-    print(book_text)
+    num_words = get_num_words(book_text)
+    print(f"{num_words} words found in the document")
 
 
 main()
