fb126e1bf2566d01a4647f436014bc3520987ee1
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2025-03-06 00:03:13 -0500

    f   - provide character frequency report and accept args

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
+
 from stats import get_num_words
+from stats import get_character_frequency
+from stats import sort_char_freq
 
 
 def get_book_text(file_path):
@@ -7,9 +11,24 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
-    num_words = get_num_words(book_text)
-    print(f"{num_words} words found in the document")
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
 
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+
+    num_words = get_num_words(book_text)
+    char_freq_dict = get_character_frequency(book_text)
+    sorted_char_freq = sort_char_freq(char_freq_dict)
+
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {book_path}')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('--------- Character Count -------')
+    for char_dict in sorted_char_freq:
+        print(f'{char_dict["character"]}: {char_dict["count"]}')
+    print('============= END ===============')
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,26 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+
+def get_character_frequency(book_text):
+    lowercase_text = book_text.lower()
+    char_freq_dict = {}
+
+    for char in lowercase_text:
+        char_freq_dict[char] = char_freq_dict.setdefault(char, 0) + 1
+
+    return char_freq_dict
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def sort_char_freq(char_freq_dict):
+    frequencies = []
+
+    for key in char_freq_dict:
+        frequencies.append({"character": key, "count": char_freq_dict[key]})
+
+    frequencies.sort(key=sort_on, reverse=True)
+    return frequencies
